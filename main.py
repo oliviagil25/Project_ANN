753c60919832685981614f8853f5264c75f26a2f
oliviagil25 <olivv2525@gmail.com>
2024-01-11 14:54:53 +0100

    SSN

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+#FUNKCJE AKTYWACJI
 # Funkcja sigmoidalna
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -36,13 +37,13 @@ Y = Y.reshape(-1, 1)
 for i in range(np.shape(X)[1]):
     X[:, i] = (X[:, i]-min(X[:,i]))/(max(X[:,i])-min(X[:,i]))
 
-trening_X = X[:800]
-trening_Y = Y[:800]
+training_X = X[:800]
+training_Y = Y[:800]
 
 testing_X = X[800:]
 testing_Y = Y[800:]
 
-
+"""
 # Inicjalizacja wag i biasu losowymi wartościami
 #ODTĄD
 np.random.seed(1)
@@ -57,10 +58,9 @@ hidden2_weights = np.random.uniform(size=(hidden1_neurons, hidden2_neurons))
 hidden2_bias = np.random.uniform(size=(1, hidden2_neurons))
 output_weights = np.random.uniform(size=(hidden2_neurons, output_neurons))
 output_bias = np.random.uniform(size=(1, output_neurons))
+"""
 
-#DOTĄD JEST DO WYWALENIA BO ZROBIŁ TO CHAT A NIŻEJ ZROBIŁAM TO SAMO TYLKO ŻE DLA RÓŻNEJ LICZBY
-#WARSTW I NEURONÓW, WIĘC JAK POTEM ROBICIE COŚ TO DOBRZE ŻEBY ODWOŁYWAŁO SIĘ DO TEGO CO ZROBIŁAM NIŻEJ
-#num_neurons jest wektorem mówiącym ile w każdej warstwie jest neuronów
+#Funkcja zwraca losowe wartości wag i biasów dla kolejnych warstw ukrytych oraz dla warstwy wyjścia
 def create_layers(num_of_neurons):
     list_weights = []
     list_bias = []
@@ -69,22 +69,16 @@ def create_layers(num_of_neurons):
         list_bias.append(np.random.uniform(size=(1, num_of_neurons[i])))
     return list_weights, list_bias
 
-#Podajemy ile jest neuronów w której warstwie a zarazem ile jest warstw(len(layers))
-#W naszym przypadku ważne, żeby pierwsza warstwa miała 12 neuronów i ostatnia 1 a reszte będzie trzeba testować
-layers = create_layers([12,4,3,1])
 
+#Trening sieci neuronowej składa się z czterech etapów: propagacja w przód, obliczenie błędu,
+# wsteczna propagacja błędu oraz aktualizacja wag i biasów
 
-#to jest taki wskaźnik jak mocno zmieniamy wagi przy każdej iteracji
-learning_rate = 0.1
-#to jest właściwie liczba iteracji przy trenowaniu
-epochs = 10000
-
-
-# Trening sieci neuronowej za pomocą wstecznej propagacji błędu
-def forward_propagation(X, layers):
+#Propagacja w przód
+#Funkcja jako parametr przyjmuje listę wag i biasów utworzoną przez funkcję create_layers()
+def forward_propagation(layers):
     activation = []
     output = []
-    activation.append(np.dot(X, layers[0][0])+layers[1][0])
+    activation.append(np.dot(training_X, layers[0][0])+layers[1][0])
     output.append(sigmoid(activation[0]))
     for i in range(1, len(layers[0])):
         activation.append(np.dot(output[i-1], layers[0][i])+layers[1][i])
@@ -92,34 +86,42 @@ def forward_propagation(X, layers):
 
     return activation, output
 
-fp = forward_propagation(X, layers)
-
-# Obliczenie błędu
-e = (Y - output[-1]) ** 2
-
-# output to fp[1] a weights to layers[0]
+# Obliczenie błędu  i wsteczna propagacja
+#Funkcja zwraca błąd dla każdej warstwy oraz iloczyn błędu i pochodnej funkcji
 def backpropagation(output, weights):
     error = []
     derrivative = []
-    error.append((Y - output[-1]) ** 2)
-    derrivative.append(error * d_sigmoid(output[-1]))
-    for i, j in zip(range(len(layers[0] - 1, -1, -1)), range(0, len(layers[0]) - 1)):
+    error.append(training_Y - output[-1]**2)
+    derrivative.append(error * sigmoid_derivative(output[-1]))
+    for i, j in zip(range(len(layers[0]) - 1, -1, -1), range(0, len(layers[0]) - 1)):
         error.append(derrivative[j].dot(weights[i].T))
-        derrivative.append(error[j + 1] * d_sigmoid(output[i - 1]))
+        derrivative.append(error[j + 1] * sigmoid_derivative(output[i - 1]))
 
     return error, derrivative
 
-back = backpropagation(fp[1], layers[0])
-
 # Aktualizacja wag i biasu
 def actualisation(layers, fp, back):
-    for i, j in zip(range(len(layers) - 1, 0, -1), range(0, len(layers) - 1)):
-        layers[0][i] += fp[1][i - 1].T.dot(back[1][j]) * learning_rate
-        layers[1][i] += np.sum(back[1][j], axis=0, keepdims=True) * learning_rate
-    layers[0][0] += X.T.dot(back[1][-1]) * learning_rate
-    layers[1][0] += np.sum(back[1][-1], axis=0, keepdims=True) * learning_rate
+    for i, j in zip(range(len(layers[0])-1, 0, -1), range(0, len(layers[0])-1)):
+        a,b = fp[1][i-1].T.dot(back[1][j]).shape[0], fp[1][i-1].T.dot(back[1][j]).shape[2]
+        layers[0][i] += fp[1][i-1].T.dot(back[1][j]).reshape(a,b) * learning_rate
+        c, d = back[1][j].shape[1], back[1][j].shape[2]
+        layers[1][i] += np.sum(back[1][j].reshape(c, d), axis=0, keepdims=True) * learning_rate
+    c, d = back[1][-1].shape[1], back[1][-1].shape[2]
+    layers[0][0] += training_X.T.dot((back[1][-1]).reshape(c, d)) * learning_rate
+    layers[1][0] += np.sum(back[1][-1].reshape(c, d), axis=0, keepdims=True) * learning_rate
+
+learning_rate = 0.1
+epochs = 10000
+#Testujemy dla dwóch warstw ukrytych o liczbie neuronów 4 i 3
+layers = create_layers([12, 4, 3, 1])
+
+for epoch in range(epochs):
+    fp = forward_propagation(layers)
+    back = backpropagation(fp[1], layers[0])
+    actualisation(layers, fp, back)
 
 
+"""
 # Trening sieci neuronowej za pomocą wstecznej propagacji błędu
 for epoch in range(epochs):
     # Forward propagation
@@ -154,22 +156,24 @@ for epoch in range(epochs):
     hidden2_bias += np.sum(d_hidden2_layer, axis=0, keepdims=True) * learning_rate
     hidden1_weights += X.T.dot(d_hidden1_layer) * learning_rate
     hidden1_bias += np.sum(d_hidden1_layer, axis=0, keepdims=True) * learning_rate
-
+"""
 # Wyświetlenie wyników po treningu
 print("Wagi warstwy ukrytej 1 po treningu:")
-print(hidden1_weights)
+print(layers[0][0])
 print("\nBias warstwy ukrytej 1 po treningu:")
-print(hidden1_bias)
+print(layers[1][0])
 print("\nWagi warstwy ukrytej 2 po treningu:")
-print(hidden2_weights)
+print(layers[0][1])
 print("\nBias warstwy ukrytej 2 po treningu:")
-print(hidden2_bias)
+print(layers[1][1])
 print("\nWagi warstwy wyjściowej po treningu:")
-print(output_weights)
+print(layers[0][2])
 print("\nBias warstwy wyjściowej po treningu:")
-print(output_bias)
+print(layers[1][2])
 
 # Testowanie sieci neuronowej
+
+"""
 test_input = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 hidden1_layer_activation = np.dot(test_input, hidden1_weights)
 hidden1_layer_activation += hidden1_bias
@@ -182,10 +186,23 @@ hidden2_layer_output = sigmoid(hidden2_layer_activation)
 output_layer_activation = np.dot(hidden2_layer_output, output_weights)
 output_layer_activation += output_bias
 predicted_output = sigmoid(output_layer_activation)
+"""
+def testing(layers):
+    activation = []
+    output = []
+    activation.append(np.dot(testing_X, layers[0][0])+layers[1][0])
+    output.append(sigmoid(activation[0]))
+    for i in range(1, len(layers[0])):
+        activation.append(np.dot(output[i-1], layers[0][i])+layers[1][i])
+        output.append(sigmoid(activation[i]))
+
+    return activation, output
+
+print(testing(layers)[1][-1])
 
 
 if __name__ == '__main__':
     print("\nWyniki po testowaniu:")
-    print(predicted_output)
+    print(testing(layers)[1][-1])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
